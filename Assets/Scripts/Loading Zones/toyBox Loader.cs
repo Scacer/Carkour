39abb1763e0d0c5830180249b456ed5264af90b4
Scacer <60669273+Scacer@users.noreply.github.com>
2026-04-01 11:40:28 +0100

    Scene Loading (Loading Zones)
    Added primitive functionality for scene loading, at current a box collider is used as a trigger, which then loads the specified scene.

--- Assets/Scripts/Loading Zones/toyBox Loader.cs
+++ Assets/Scripts/Loading Zones/toyBox Loader.cs
@@ -1,22 +1,14 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class toyBoxLoader : MonoBehaviour
 {
-    public static event Action;
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.localRotation = Quaternion.Euler(90f, Time.time * 100f, 0);
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            OnCollected?.Invoke();
-            Destroy(gameObject);
+            SceneManager.LoadScene("Toybox");
         }
     }
 }
